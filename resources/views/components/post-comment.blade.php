cf6c5410ee2791308a67fb9336bc4575491abf3d
marc@plusnarrative.com <marc@plusnarrative.com>
2022-03-16 09:34:41 +0200

    comment updates

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,10 +1,10 @@
 @props(['comment'])
 <article class="flex bg-gray-100 p-6 rounded-xl border-gray-200 mb-4">
-    <div class="flex-shrink-0"><img src="https://i.pravatar.cc/100" alt="" width="100px" /></div>
+    <div class="flex-shrink-0"><img src="https://i.pravatar.cc/60?={{ $comment->id }}" alt="" width="60px" class="rounded-xl" /></div>
     <div class="ml-4">
         <header class="">
             <h3 class="font-bold">{{ $comment->author->username }}</h3>
-            <p class="text-xs">Posted <time>8 Months ago</time></p>
+            <p class="text-xs">Posted <time>{{ $comment->created_at->diffForHumans() }}</time></p>
         </header>
         <p>
           {!! $comment->body !!}
